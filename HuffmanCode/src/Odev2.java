ef12bc107e85418cd5a9df7038f92cdc4e9c2890
nurselincanturk <nurselincanturk@outlook.com>
2020-09-07 15:52:22 +0300

    Add files via upload

---
+++ HuffmanCode/src/Odev2.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class Odev2 {
+    /**
+     * huffmanKodla metodu verilen girdi metnini bir Huffman ağacı oluşturarak kodlar. Aynı zamanda parametre olarak gönderilen Huffman ağacını doldurur. Test işlemleri geri dönen kod ve ağaç üzerinden gerçekleştirilmektedir.
+     * @param girdi Girdi metni
+     * @param agac boş olarak verilen ağaç(HuffmanTree)
+     * @return kodlanmış metni döndürür
+     */
+    public static String huffmanKodla(String girdi, HuffmanTree agac) {
+        String[] huffmanNode=girdi.split(" ");
+        
+      HuffmanTree tree=new HuffmanTree();
+      tree.addAll(huffmanNode)
+              
+              
+              ;
+        
+        
+        return null;
+    }
+}
---
+++ HuffmanCode/src/huffmancode/HuffmanCode.java
@@ -0,0 +1,11 @@
+
+package huffmancode;
+
+
+public class HuffmanCode {
+
+    public static void main(String[] args) {
+      
+    }
+    
+}
